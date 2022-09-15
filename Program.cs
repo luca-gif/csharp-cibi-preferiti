f1091f727fff1de45421509f55a564082d6fe2f7
luca-gif <luca.89m@gmail.com>
2022-09-15 15:36:06 +0200

    punto 4

--- Program.cs
+++ Program.cs
@@ -16,3 +16,8 @@ Console.WriteLine(food);
 //.3 Il vostro cibo top (prima posizione della classifica)
 
 Console.WriteLine("cibo top: " + myFoods[0]);
+
+//.4 Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+
+Console.WriteLine("cibo non troppo top: " + myFoods[5]);
+
