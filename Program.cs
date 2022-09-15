4a911e0b991c1f751228005100ec02d6b0e230b6
luca-gif <luca.89m@gmail.com>
2022-09-15 16:18:56 +0200

    Aggiunto bonus

--- Program.cs
+++ Program.cs
@@ -19,5 +19,19 @@ Console.WriteLine("cibo top: " + myFoods[0]);
 
 //.4 Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
 
-Console.WriteLine("cibo non troppo top: " + myFoods[5]);
+Console.WriteLine("ultimo cibo: " + myFoods[myFoods.Length -1]);
 
+// Bonus Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana.
+// Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari.
+// In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
+
+
+if (myFoods.Length % 2 == 1)
+{
+    Console.WriteLine(myFoods[myFoods.Length / 2]);
+}
+else
+{
+    Console.WriteLine(myFoods[myFoods.Length / 2]);
+    Console.WriteLine(myFoods[myFoods.Length / 2 - 1]);
+}
