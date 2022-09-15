34e2aa642a0150b8964d50b93ef74e89d02f1afa
luca-gif <luca.89m@gmail.com>
2022-09-15 15:28:18 +0200

    Terminati punti 1,2,3.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-//primo commit
+
+string[] myFoods = {"pizza", "pasta", "polpette", "picanha", "pollo", "formaggio"};
+
+//.1 La lunghezza della classifica
+Console.WriteLine("Lunghezza Array: " + myFoods.Length);
+
+
+//.2 La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+
+foreach (var food in myFoods)
+
+Console.WriteLine(food);
+
+//.3 Il vostro cibo top (prima posizione della classifica)
+
+Console.WriteLine("cibo top: " + myFoods[0]);
